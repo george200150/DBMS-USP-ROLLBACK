53b6fe75e7c9ecccea8f62607e55d6d6dda642f8
George Ciubotariu <georgeciubotariu@yahoo.com>
2020-04-08 21:32:23 +0300

    added the sql + c# code
    - one stored procedure with full rollback in case of error
    - one stored procedure with progressive checkpoints
    - created own logging system
    - validated the data before beginning the transactions

---
+++ SGBDlab3/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SGBDlab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.Size = new System.Drawing.Size(692, 217);
+            this.dataGridViewParent.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 495);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Populeaza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 478);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Adauga roll";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(12, 249);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.Size = new System.Drawing.Size(309, 217);
+            this.dataGridViewChild.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(428, 481);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(428, 507);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 484);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "@titlu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 510);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "@gen";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(327, 249);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(377, 217);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(244, 507);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Adauga check";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(710, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ShowEditingIcon = false;
+            this.dataGridView2.Size = new System.Drawing.Size(692, 520);
+            this.dataGridView2.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1414, 544);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ SGBDlab3/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace SGBDlab3
+{
+
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-A1S24IB\SQLEXPRESS;Initial Catalog=movie;Integrated Security=True");
+        //SqlConnection connection = new SqlConnection("Data Source=DESKTOP-A1S24IB\\SQLEXPRESS;Initial Catalog=...;Integrated Security=True");
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlDataAdapter adapter2 = new SqlDataAdapter();
+        SqlDataAdapter adapter3 = new SqlDataAdapter();
+        SqlDataAdapter adapter4 = new SqlDataAdapter();
+        DataSet data = new DataSet();
+        DataSet data2 = new DataSet();
+        DataSet data3 = new DataSet();
+        DataSet data4 = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewParent.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewChild.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void updateThis()
+        {
+            adapter.SelectCommand = new SqlCommand("SELECT * FROM Filme", connection);
+            data.Clear();
+            adapter.Fill(data);
+            dataGridViewParent.DataSource = data.Tables[0];//luam tabelul returnat de query
+
+
+            adapter2.SelectCommand = new SqlCommand("SELECT * FROM Critica", connection);
+            data2.Clear();
+            adapter2.Fill(data2);
+            dataGridViewChild.DataSource = data2.Tables[0];//luam tabelul returnat de query
+
+
+            adapter3.SelectCommand = new SqlCommand("SELECT * FROM Genuri", connection);
+            data3.Clear();
+            adapter3.Fill(data3);
+            dataGridView1.DataSource = data3.Tables[0];//luam tabelul returnat de query
+        }
+
+        private void button1_Click(object sender, EventArgs e) // - să se afişeze toate înregistrările tabelei părinte;
+        {
+            updateThis();
+
+            adapter4.SelectCommand = new SqlCommand("SELECT * FROM Logger", connection);
+            data4.Clear();
+            adapter4.Fill(data4);
+            dataGridView2.DataSource = data4.Tables[0];//luam tabelul returnat de query
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand cmd = new SqlCommand("usp_lab3_sgbd_rollback", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@titlu", textBox1.Text);
+            cmd.Parameters.AddWithValue("@gen", textBox2.Text);
+            connection.Open();
+            try
+            {
+                int rowAffected2 = cmd.ExecuteNonQuery();
+                MessageBox.Show(rowAffected2 + " affected rows by the procedure");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            connection.Close();
+
+            adapter4.SelectCommand = new SqlCommand("SELECT * FROM Logger", connection);
+            data4.Clear();
+            adapter4.Fill(data4);
+            dataGridView2.DataSource = data4.Tables[0];//luam tabelul returnat de query
+
+            SqlCommand cmd2 = new SqlCommand("usp_lab3_sgbd_teardown", connection);
+            cmd2.CommandType = CommandType.StoredProcedure;
+            connection.Open();
+            int rowAffected = cmd2.ExecuteNonQuery();
+            MessageBox.Show(rowAffected + " terminated aborted procedures");
+            connection.Close();
+
+            updateThis();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("usp_lab3_sgbd_checkpoint", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@titlu", textBox1.Text);
+            cmd.Parameters.AddWithValue("@gen", textBox2.Text);
+            connection.Open();
+            try
+            {
+                int rowAffected2 = cmd.ExecuteNonQuery();
+                MessageBox.Show(rowAffected2 + " affected rows by the procedure");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            connection.Close();
+
+            adapter4.SelectCommand = new SqlCommand("SELECT * FROM Logger", connection);
+            data4.Clear();
+            adapter4.Fill(data4);
+            dataGridView2.DataSource = data4.Tables[0];//luam tabelul returnat de query
+
+            SqlCommand cmd2 = new SqlCommand("usp_lab3_sgbd_teardown", connection);
+            cmd2.CommandType = CommandType.StoredProcedure;
+            connection.Open();
+            int rowAffected = cmd2.ExecuteNonQuery();
+            MessageBox.Show(rowAffected + " terminated aborted procedures");
+            connection.Close();
+
+            updateThis();
+        }
+    }
+}
